eee4d5218bb2939d205307e8aad2f873b8ebe02a
Ugochukwu Odunukwe <55397041+UgoOluwa@users.noreply.github.com>
2020-02-19 12:30:44 +0100

    Added cloudinary upload video path

--- E-Commerce/Controllers/AdminController.cs
+++ E-Commerce/Controllers/AdminController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using System.Web.Mvc;
+using E_Commerce.Models;
 
 namespace E_Commerce.Controllers
 {
@@ -10,8 +13,45 @@ namespace E_Commerce.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+
+
         public ActionResult AdminHome()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdminHome(Movies movie)
+        {
+            try
+            {
+                Account account = new Account(
+                    "zorn",
+                    "834965577395824",
+                    "uctoPzHnQMZdz_A-MYWp2YLxhVk");
+
+                Cloudinary cloudinary = new Cloudinary(account);
+
+                var uploadParams = new VideoUploadParams()
+                {
+                    File = new FileDescription($@"{movie.Uri}"),
+                    PublicId = $"Videos/{movie.Name}",
+                    EagerAsync = true,
+                    EagerNotificationUrl = "https://mysite.example.com/my_notification_endpoint"
+                };
+                var uploadResult = cloudinary.UploadLarge(uploadParams);
+                movie.Uri = uploadResult.Uri.ToString();
+
+                var context = new ApplicationDbContext();
+                context.Movies.Add(movie);
+                context.SaveChanges();
+                ViewBag.Success = "Movie Added Successfully";
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+            }
+
             return View();
         }
 
